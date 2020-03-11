4d91b153e3502fbbdcfaa1b6e1df6032b9bf4719
Yauhen.Masalkou <haslem11@gmail.com>
2020-03-11 17:37:57 +0100

    Add Recycler View (ListView plus customAdapter)

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,12 +2,38 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+
+
+
+        //android.R.layout.simple_list_item_1  - predefined by Android
+        //it is one of predefined layouts in R.layout
+        //android.R.layout.simple_list_item_1 - predefined layout, that contains one textview
+        //https://developer.android.com/reference/android/R.layout
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,12 +2,42 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("one", "miwok1"));
+        words.add(new Word("two", "miwok2"));
+        words.add(new Word("three", "miwok3"));
+        words.add(new Word("four", "miwok4"));
+        words.add(new Word("five", "miwok5"));
+        words.add(new Word("six", "miwok6"));
+        words.add(new Word("seven", "miwok7"));
+        words.add(new Word("eight", "miwok8"));
+        words.add(new Word("nine", "miwok9"));
+
+
+        //android.R.layout.simple_list_item_1  - predefined by Android
+        //it is one of predefined layouts in R.layout
+        //android.R.layout.simple_list_item_1 - predefined layout, that contains one textview
+        //https://developer.android.com/reference/android/R.layout
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+
+
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+
     }
 }
